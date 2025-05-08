b030d2f1997380074ef9cf963d9e275c64641ca2
Johnviley <125841622+Johnviley@users.noreply.github.com>
2025-05-08 15:45:37 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,13 +1,9 @@
 <?php
 
-$host = getenv("DB_HOST");
-$user = getenv("DB_USER");
-$pass = getenv("DB_PASS");
-$name = getenv("DB_NAME");
+    $database = new mysqli("192.185.48.158", "bisublar_clinic", "Cl1n1c2025@", "bisublar_clinic");
+    
+    if ($database->connect_error) {
+        die("Connection failed: " . $database->connect_error);
+    }
 
-$database = new mysqli($host, $user, $pass, $name);
-
-if ($database->connect_error) {
-    die("Connection failed: " . $database->connect_error);
-}
 ?>
